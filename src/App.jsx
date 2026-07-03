eaa1efad69157e9bd4d0f42ea757aff1e135249b
Fatimah Hassan <fattyhassan14@gmail.com>
2026-07-02 23:51:42 -0400

    fix: formatted the Array in App.jsx based on the Airhub review

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     {id: 1, title: "review resources"},
     {id: 2, title: "take notes"},
     {id: 3, title: "code out app"},
-]
+  ]
   return (
     <div>
         <h1>Todo List</h1>
