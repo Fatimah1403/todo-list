9be5c477abe8200d4c8ee4321dc8babea4283b8d
Fatimah Hassan <fattyhassan14@gmail.com>
2026-07-11 15:04:36 -0400

    (feat): created the component for the list and form

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TodoList = () => {
+    const todoList = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+  ]
+  return (
+    <div>
+        <ul>
+            {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        </ul>
+    </div>
+  )
+}
+
+export default TodoList
