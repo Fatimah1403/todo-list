71550923fea416ff73f883aa3e7cbad21724959f
Fatimah Hassan <fattyhassan14@gmail.com>
2026-08-02 14:51:49 -0400

    feat: add conditional rendering, todo completion, and controlled form

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,20 @@
 import TodoListItem from "./TodoListItem"
-const TodoList = ({todoList}) => {
+const TodoList = ({todoList, onCompleteTodo}) => {
+  const filteredTodoList = todoList.filter(todo => !todo.isCompleted)
   return (
     <div>
+     {filteredTodoList.length === 0 ? (
+      <p>Add todo above to get started</p>
+     ) : (
       <ul>
-        {todoList.map(todo => <TodoListItem key={todo.id} todo={todo}/>)}
+        {filteredTodoList.map(todo => (
+          <TodoListItem key={todo.id} 
+          todo={todo} 
+          onCompleteTodo={onCompleteTodo}
+        />
+      ))}
       </ul>
+     )}
     </div>
   )
 }
