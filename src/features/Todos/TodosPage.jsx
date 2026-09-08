472dc8f4df1aab2113a5765ff5d361a113eeaab0
Fatimah Hassan <fattyhassan14@gmail.com>
2026-09-08 10:27:56 -0400

    feat: implement lesson 8 useMemo and useCallback optimizations

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -19,6 +19,7 @@ const TodosPage = ({ token }) => {
 
  const invalidateCache = useCallback(() => {
     // console.log('Invalidating memo cache after todo mutation');
+    
 
     setDataVersion(prev => prev + 1);
   }, []);
